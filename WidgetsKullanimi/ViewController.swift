e5c56f33c0a5dd230986562d7fda7bb421e320e6
atilla.citirik <atillactrk@gmail.com>
2024-05-08 22:32:50 +0300

    Resim değişikliği ve textfield veri okuma

--- WidgetsKullanimi/ViewController.swift
+++ WidgetsKullanimi/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelSonuc: UILabel!
+    @IBOutlet weak var textFieldGirdi: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonYap(_ sender: Any) {
+        if let alinanVeri = textFieldGirdi.text {
+            labelSonuc.text = alinanVeri
+        }
+    }
+    
+    @IBAction func buttonResim1(_ sender: Any) {
+        imageView.image = UIImage(named: "resim1")
+    }
+    
+    @IBAction func buttonResim2(_ sender: Any) {
+        imageView.image = UIImage(named: "resim2")
+    }
 }
 
